b284d99381c147ac2124a417ec55550811a14574
kumari.chavali <kumari.chavali@object-frontier.com>
2017-08-07 18:55:48 +0530

    comments UI design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TitlebarComponent } from './titlebar/titlebar.component';
@@ -18,7 +19,7 @@ import { CommentboxComponent } from './commentbox/commentbox.component';
     CommentboxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/commentbox/commentbox.component.ts
+++ src/app/commentbox/commentbox.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./commentbox.component.css']
 })
 export class CommentboxComponent implements OnInit {
-
+  public commentInput : String ="";
+  public isDisabled : boolean = true;
   constructor() { }
 
   ngOnInit() {
   }
-
+  enableSubmit(){
+    if(this.commentInput.length>0){
+        this.isDisabled = false;
+    }
+    console.log("data..."+this.commentInput + this.isDisabled);
+  }
 }
