2d96ecf1db77682bffd0a2a833d346d8e962cb6b
OFS1\rajeswari.babu <rajeswari.babu@object-frontier.com>
2017-08-11 12:41:43 +0530

    Comments view added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component,Input } from '@angular/core';
+import { CommentService } from './commentbox/comment.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-
 })
 export class AppComponent {
-  public childData;
+  public childData ='https://www.youtube.com/embed/GU-2T7k9NfI?list=PL55RiY5tL51rcCnrOrZixuOsZhAHHy6os';
   ngOnInit() {
 }
   
--- src/app/commentbox/commentbox.component.ts
+++ src/app/commentbox/commentbox.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentService } from './comment.service';
+
 
 @Component({
   selector: 'app-commentbox',
   templateUrl: './commentbox.component.html',
-  styleUrls: ['./commentbox.component.css']
+  styleUrls: ['./commentbox.component.css'],
+  providers: [CommentService]
+
 })
 export class CommentboxComponent implements OnInit {
   public commentInput : String ="";
   public isDisabled : boolean = true;
-  constructor() { }
+  errorMsg : String;
+  commentList = [];
+  constructor(private _commentService : CommentService) { }
 
   ngOnInit() {
+
+   this._commentService.getComments().subscribe(resCommentData => this.commentList = resCommentData
+,resCommentError => this.errorMsg = resCommentError);
+ 
   }
   enableSubmit(){
     if(this.commentInput.length>0){
