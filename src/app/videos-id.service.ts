acd2dd3a4f13e0f46f89e54d5c08afd8f4db5c57
Ganesh <ganeshbabu.appalanaidu@object-frontier.com>
2017-08-16 20:10:55 +0530

    fetch data from json to display for comment box.

--- src/app/videos-id.service.ts
+++ src/app/videos-id.service.ts
@@ -11,18 +11,28 @@ import {Observable} from 'rxjs/Rx';
 export class VideoIdService{
 
     private _videourl: string = "assets/videos.json"
+     private _videoIDUrl: string = "assets/";
     constructor(private _http: Http){} 
 
     getVideoPlayerData(): Observable<VideoObject[]>{
         return this._http.get(this._videourl)
-            .map((response:Response) =><VideoObject[]> response.json());
+            .map((response:Response) =><VideoObject[]> response.json())
+             .catch(this._errorHandler);
     }
 
-    // fetchVideoCommentsData(filename): Observable<VideoCommentModel[]>{
+  
 
-    //     private  _videoIdCommetData: string ="assets/"
-    // }
+   getComments(fileName):Observable<VideoCommentModel[]>{
+        // this._videoIDUrl = this._videoIDUrl + fileName + "."+ "json";
+         console.log ("filename :"+ fileName);
+        console.log ("VideoIdurl is :"+ this._videoIDUrl);
+    return this._http.get(fileName).map((response:Response)=><VideoCommentModel[]> response.json()).catch(this._errorHandler);
     
+   }
+     _errorHandler(error:Response){
+        console.error(error);
+        return Observable.throw(error || "Server Error")
+    }
 
 
 }
