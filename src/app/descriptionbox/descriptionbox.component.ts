1705c9b106b73df8bd7bed61d4410123c2fd59ba
Ganesh <ganeshbabu.appalanaidu@object-frontier.com>
2017-08-21 16:09:18 +0530

    Likes and dislikes count implemented of the video.

--- src/app/descriptionbox/descriptionbox.component.ts
+++ src/app/descriptionbox/descriptionbox.component.ts
@@ -7,12 +7,22 @@ import { VideoObject } from '../video-object';
   styleUrls: ['./descriptionbox.component.css']
 })
 export class DescriptionboxComponent implements OnInit {
-  @Input() videoObject:VideoObject;
-  
+  @Input() videoObject: VideoObject;
+
   constructor() { }
 
   ngOnInit() {
-    console.log(this.videoObject);
+    console.log('description video obJect:' + this.videoObject);
+  }
+  likesincrementUp() {
+    this.videoObject.noOfLikes = this.videoObject.noOfLikes += 1;
+    console.log("likes up called");
+
+  }
+  dislikesdecrementDown() {
+    this.videoObject.noOfDislikes = this.videoObject.noOfDislikes += 1;
+    console.log("likes down called");
+
   }
 
 }
